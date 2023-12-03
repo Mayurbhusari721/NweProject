cd2809a8215b0d16f8e69ce90a7878c0f988d4f5
Mayurbhusari721 <mbhusari141@gmail.com>
2023-12-03 15:16:32 +0530

    my first commit

---
+++ src/farming/X.java
@@ -0,0 +1,10 @@
+package farming;
+
+public class X {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/farming/Y.java
@@ -0,0 +1,11 @@
+package farming;
+
+public class Y {
+
+	public static void main(String[] args)
+	{
+		System.out.println("cotton plant");
+
+	}
+
+}
